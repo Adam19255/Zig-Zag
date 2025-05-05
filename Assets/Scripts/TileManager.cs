8973b3e50266b22ebbe38ed71c1727732a212cb5
Evyatarbe <evyevy12@gmail.com>
2025-05-05 12:15:55 +0300

    tile color change every 50 points

--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -1,13 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class TileManager : MonoBehaviour{
+public class TileManager : MonoBehaviour {
 
     [SerializeField] private GameObject[] tilePrefabs;
     [SerializeField] private GameObject currentTile;
     [SerializeField] private GameObject startTile;
+    [SerializeField] private Transform tileParent;
 
     private Stack<GameObject> leftTiles = new Stack<GameObject>();
     private Stack<GameObject> topTiles = new Stack<GameObject>();
@@ -17,6 +17,11 @@ public class TileManager : MonoBehaviour{
     private int leftTileCount = 0;
     private int topTileCount = 0;
 
+    private Color[] tileColors = new Color[10];
+    private int currentColorIndexGlobal = -1;
+    private int currentColorTier = 0;
+    private Color currentColor;
+    private int lastAppliedTier = 0;
 
     public static TileManager Instance {
         get {
@@ -42,11 +47,17 @@ public class TileManager : MonoBehaviour{
         set { startTiles = value; }
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        AddTiles(50); // Add 50 tiles to the stack
+    void Start() {
+        SetupFixedColors();
 
+        currentColorIndexGlobal = Random.Range(0, tileColors.Length);
+        currentColor = tileColors[currentColorIndexGlobal];
+
+        if (tileParent == null) {
+            tileParent = new GameObject("TileParent").transform;
+        }
+
+        AddTiles(50); // Add 50 tiles to the stack
         // Add the startTile to the stack
         startTiles.Push(startTile);
 
@@ -56,47 +67,93 @@ public class TileManager : MonoBehaviour{
     }
 
     // Function to add tiles to the stack to recycle them
+    void Update() {
+        int score = PlayerScript.Instance.GetScore();
+        int tier = score / 50;
+
+        if (tier != lastAppliedTier && score >= 50) {
+            lastAppliedTier = tier;
+
+            int newIndex;
+            do {
+                newIndex = Random.Range(0, tileColors.Length);
+            } while (newIndex == currentColorIndexGlobal);
+
+            currentColorIndexGlobal = newIndex;
+            currentColor = tileColors[currentColorIndexGlobal];
+
+            ApplyColorToRecentTiles(currentColor);
+        }
+    }
+
+    void SetupFixedColors() {
+        tileColors = new Color[] {
+            Color.red,
+            Color.green,
+            Color.blue,
+            Color.yellow,
+            Color.black,
+            Color.magenta,
+            new Color(1f, 0.5f, 0f),           // orange
+            new Color(0.5f, 0f, 1f),           // purple
+            new Color(0f, 0.75f, 0.75f),       // teal
+            Color.gray
+        };
+    }
+
     public void AddTiles(int amount) {
         for (int i = 0; i < amount; i++) {
-            leftTiles.Push(Instantiate(tilePrefabs[0]));
-            topTiles.Push(Instantiate(tilePrefabs[1]));
-            leftTiles.Peek().name = "LeftTile"; // Set the name of the tile to "LeftTile" in the stack
-            leftTiles.Peek().SetActive(false); // Deactivate the tile so it doesn't appear in the scene
-            topTiles.Peek().name = "TopTile";
-            topTiles.Peek().SetActive(false);
+            GameObject left = Instantiate(tilePrefabs[0], tileParent);
+            GameObject top = Instantiate(tilePrefabs[1], tileParent);
+            left.name = "LeftTile";// Set the name of the tile to "LeftTile" in the stack
+            top.name = "TopTile";
+            left.SetActive(false);// Deactivate the tile so it doesn't appear in the scene
+            top.SetActive(false);
+            leftTiles.Push(left);
+            topTiles.Push(top);
         }
     }
 
     public void CreateTiles() {
         if (leftTiles.Count == 0 || topTiles.Count == 0) { // Check if the stacks are empty
-            AddTiles(10); // Add more tiles if the stacks are empty
+            AddTiles(10);// Add more tiles if the stacks are empty
         }
-        int randomIndex = Random.Range(0, tilePrefabs.Length);
 
+        int randomIndex = Random.Range(0, tilePrefabs.Length);
+        
         // Make sure there isn't a chance for more than 10 times of the same tile
         if (leftTileCount > 9) {
             randomIndex = 1;
             leftTileCount = 0;
-        }
-        else if (topTileCount > 9) {
+        } else if (topTileCount > 9) {
             randomIndex = 0;
             topTileCount = 0;
         }
 
-        // Create left and top tiles
+        GameObject newTile = null;
+
         if (randomIndex == 0) {
-            GameObject newTile = leftTiles.Pop();
+            newTile = leftTiles.Pop();
             newTile.SetActive(true);
             newTile.transform.position = currentTile.transform.GetChild(0).transform.GetChild(randomIndex).position;
             currentTile = newTile;
             leftTileCount++;
-        }
-        else if (randomIndex == 1) {
-            GameObject newTile = topTiles.Pop();
+        } else if (randomIndex == 1) {
+            newTile = topTiles.Pop();
             newTile.SetActive(true);
             newTile.transform.position = currentTile.transform.GetChild(0).transform.GetChild(randomIndex).position;
             currentTile = newTile;
             topTileCount++;
+        }
+
+        // Apply latest color to new tile if score >= 50
+        int currentScore = PlayerScript.Instance.GetScore();
+        if (currentScore >= 50) {
+            Renderer tileRenderer = newTile.transform.GetChild(0).GetComponent<Renderer>();
+            if (tileRenderer != null) {
+                tileRenderer.material = new Material(tileRenderer.material);
+                StartCoroutine(LerpColor(tileRenderer.material, currentColor, 0.5f));
+            }
         }
 
         // Create tiles with gems on it
@@ -104,5 +161,39 @@ public class TileManager : MonoBehaviour{
         if (createGem == 0) {
             currentTile.transform.GetChild(1).gameObject.SetActive(true);
         }
+    }
+
+    private void ApplyColorToRecentTiles(Color color) {
+        if (tileParent == null) {
+            Debug.LogError("[TILE MANAGER] tileParent not assigned!");
+            return;
+        }
+
+        foreach (Transform tile in tileParent) {
+            if (!tile.gameObject.activeInHierarchy) continue;
+
+            if (tile.childCount > 0) {
+                Transform meshHolder = tile.GetChild(0);
+
+                Renderer tileRenderer = meshHolder.GetComponent<Renderer>();
+                if (tileRenderer != null) {
+                    tileRenderer.material = new Material(tileRenderer.material);
+                    StartCoroutine(LerpColor(tileRenderer.material, color, 0.5f));
+                }
+            }
+        }
+    }
+
+    private IEnumerator LerpColor(Material mat, Color targetColor, float duration) {
+        Color startColor = mat.color;
+        float time = 0f;
+
+        while (time < duration) {
+            mat.color = Color.Lerp(startColor, targetColor, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        mat.color = targetColor;
     }
 }
