fecda81b5e2f8592f940bc30e5c67a0ecc352a82
Evyatarbe <evyevy12@gmail.com>
2025-05-25 18:14:28 +0300

    shop complete

--- Assets/Scripts/ShopManager.cs
+++ Assets/Scripts/ShopManager.cs
@@ -9,6 +9,22 @@ public class ShopManager : MonoBehaviour
     public Transform shopGridParent;  // Assign ShopGrid
     public List<SkinItem> skinItems;
 
+    // Icon sprites
+    private Sprite iconEquipped, iconEquip, iconBuy;
+
+    private List<TMP_Text> allButtonTexts = new List<TMP_Text>();
+    private List<Image> allButtonImages = new List<Image>();
+    private List<SkinItem> allSkinItems = new List<SkinItem>();
+    private List<Image> allIconImages = new List<Image>();
+
+    void Awake()
+    {
+        // Load icons from Resources/Images/Icons/
+        iconEquipped = Resources.Load<Sprite>("Icons/v");
+        iconEquip    = Resources.Load<Sprite>("Icons/play");
+        iconBuy      = Resources.Load<Sprite>("Icons/GemImage");
+    }
+
     void Start()
     {
         PopulateShopItems();
@@ -19,30 +35,68 @@ public class ShopManager : MonoBehaviour
         foreach (var item in skinItems)
         {
             GameObject obj = Instantiate(shopItemPrefab, shopGridParent);
-            
+
+            // Background tint by tier
+            Image bgImage = obj.GetComponent<Image>();
+            Color tierColor = Color.white;
+            switch (item.tier.ToLower())
+            {
+                case "common":    tierColor = new Color(0.8f, 0.8f, 0.8f); break;
+                case "rare":      tierColor = new Color(0.4f, 0.6f, 1f); break;
+                case "epic":      tierColor = new Color(0.7f, 0.3f, 0.9f); break;
+                case "legendary": tierColor = new Color(1f, 0.65f, 0f); break;
+            }
+            bgImage.color = tierColor;
+
+            // UI References
             RawImage preview = obj.transform.Find("SkinPreview").GetComponent<RawImage>();
             TMP_Text tierText = obj.transform.Find("TierText").GetComponent<TMP_Text>();
-            TMP_Text priceText = obj.transform.Find("PriceText").GetComponent<TMP_Text>();
             TMP_Text buttonText = obj.transform.Find("BuyButton/BuyButtonText").GetComponent<TMP_Text>();
             Button buyButton = obj.transform.Find("BuyButton").GetComponent<Button>();
+            Button previewButton = obj.transform.Find("SkinPreview").GetComponent<Button>();
+            Image buttonImage = buyButton.GetComponent<Image>();
+            Image iconImage = obj.transform.Find("BuyButton/StateIcon").GetComponent<Image>();
 
+            // Setup visuals
             preview.texture = item.skinTexture;
             tierText.text = item.tier;
-            priceText.text = $"{item.cost} Gems";
 
             item.isOwned = PlayerPrefs.GetInt("OwnedSkin_" + item.skinName, 0) == 1;
+            string equipped = PlayerPrefs.GetString("EquippedSkin", "");
 
             if (item.isOwned)
             {
-                string equipped = PlayerPrefs.GetString("EquippedSkin", "");
-                buttonText.text = item.skinName == equipped ? "Equipped" : "Equip";
+                if (item.skinName == equipped)
+                {
+                    buttonText.text = "Equipped";
+                    buttonImage.color = Color.green;
+                    iconImage.sprite = iconEquipped;
+                }
+                else
+                {
+                    buttonText.text = "Equip";
+                    buttonImage.color = new Color(0.2f, 0.4f, 1f);
+                    iconImage.sprite = iconEquip;
+                }
             }
             else
             {
-                buttonText.text = "Buy";
+                buttonText.text = $"{item.cost} x Gems";
+                buttonImage.color = Color.white;
+                iconImage.sprite = iconBuy;
             }
 
+            iconImage.enabled = true;
+
+            // Button actions
             buyButton.onClick.AddListener(() => BuySkin(item, buttonText));
+            previewButton.onClick.AddListener(() => BuySkin(item, buttonText));
+
+            // Track for refresh
+            allButtonTexts.Add(buttonText);
+            allButtonImages.Add(buttonImage);
+            allIconImages.Add(iconImage);
+            allSkinItems.Add(item);
         }
     }
 
@@ -52,23 +106,49 @@ public class ShopManager : MonoBehaviour
 
         if (!item.isOwned && currentGems >= item.cost)
         {
+            // Buy skin
             currentGems -= item.cost;
             PlayerPrefs.SetInt("Gems", currentGems);
-            PlayerPrefs.Save();
-            PowerUpUI.Instance.UpdateGems(currentGems);
-
-            item.isOwned = true;
             PlayerPrefs.SetInt("OwnedSkin_" + item.skinName, 1);
             PlayerPrefs.Save();
 
-            buttonText.text = "Equip";
-            Debug.Log($"Bought {item.skinName}");
+            item.isOwned = true;
+            PowerUpUI.Instance.UpdateGems(currentGems);
         }
-        else if (item.isOwned)
+
+        if (item.isOwned)
         {
+            // Equip skin
             PlayerPrefs.SetString("EquippedSkin", item.skinName);
             PlayerPrefs.Save();
-            buttonText.text = "Equipped";
+
+            for (int i = 0; i < allSkinItems.Count; i++)
+            {
+                bool isEquipped = allSkinItems[i].skinName == item.skinName;
+                bool isOwned = allSkinItems[i].isOwned;
+
+                if (isEquipped)
+                {
+                    allButtonTexts[i].text = "Equipped";
+                    allButtonImages[i].color = Color.green;
+                    allIconImages[i].sprite = iconEquipped;
+                }
+                else if (isOwned)
+                {
+                    allButtonTexts[i].text = "Equip";
+                    allButtonImages[i].color = new Color(0.2f, 0.4f, 1f);
+                    allIconImages[i].sprite = iconEquip;
+                }
+                else
+                {
+                    allButtonTexts[i].text = $"{allSkinItems[i].cost} x Gems";
+                    allButtonImages[i].color = Color.white;
+                    allIconImages[i].sprite = iconBuy;
+                }
+
+                allIconImages[i].enabled = true;
+            }
+
             Debug.Log($"Equipped {item.skinName}");
         }
         else
